f2539cea73fc881975031552b5809fad7b0b5169
francescabarnes <francescabarnes@protonmail.com>
2023-06-23 09:34:10 +0200

    first commit folders and structure

---
+++ Timezone/script.js
@@ -0,0 +1,18 @@
+// Find the timezones of :
+
+// Anchorage (USA)
+// Reykjavik (Iceland)
+// Saint-Petersburg (Russia)
+// And display the date and time of these cities along with the time and date of Brussels.
+
+let date = new Date();
+let utc = date.getTime() + date.getTimezoneOffset() * 60000;
+let brussels = new Date(utc + 3600000 * 2);
+let anchorage = new Date(utc + 3600000 * -8);
+let reykjavik = new Date(utc + 3600000 * 0);
+let saintPetersburg = new Date(utc + 3600000 * 3);
+
+console.log("Brussels : " + brussels);
+console.log("Anchorage : " + anchorage);
+console.log("Reykjavik : " + reykjavik);
+console.log("Saint-Petersburg : " + saintPetersburg);
