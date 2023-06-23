28c04aa033791ede10f31fd91a5cbff26e70617f
francescabarnes <francescabarnes@protonmail.com>
2023-06-23 10:50:06 +0200

    updating js and html for timezone

--- Timestamp/script.js
+++ Timestamp/script.js
@@ -1,3 +1,19 @@
 // Using timestamps, find how many days have passed since the date of your birth. Feeling old, yet?
 
 // Write a function to find how many days have passed since any point in time (after 1970).
+
+// Example:
+
+// daysPassed(new Date(2018, 0, 15)) // 173
+
+let date = new Date();
+
+function daysPassed(date) {
+  let today = new Date();
+  let days = Math.floor((today - date) / 86400000);
+  return days;
+}
+
+console.log(daysPassed(new Date(1994, 5, 5)));
+
+// Write a function to find how many seconds have passed since any point in time (after 1970).
--- Timezone/script.js
+++ Timezone/script.js
@@ -7,6 +7,7 @@
 
 let date = new Date();
 let utc = date.getTime() + date.getTimezoneOffset() * 60000;
+
 let brussels = new Date(utc + 3600000 * 2);
 let anchorage = new Date(utc + 3600000 * -8);
 let reykjavik = new Date(utc + 3600000 * 0);
@@ -16,3 +17,12 @@ console.log("Brussels : " + brussels);
 console.log("Anchorage : " + anchorage);
 console.log("Reykjavik : " + reykjavik);
 console.log("Saint-Petersburg : " + saintPetersburg);
+
+function displayTime() {
+  document.getElementById("bxlclock").innerHTML = `${brussels}`;
+  document.getElementById("ancorclock").innerHTML = `${anchorage}`;
+  document.getElementById("reyclock").innerHTML = `${reykjavik}`;
+  document.getElementById("stpclock").innerHTML = `${saintPetersburg}`;
+}
+
+displayTime();
