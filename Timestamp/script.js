b802f414f256c444f863e8a63bce4a30e3b07fc0
francescabarnes <francescabarnes@protonmail.com>
2023-06-23 10:10:30 +0200

    adding 3rd and 4th folders

---
+++ Timestamp/script.js
@@ -0,0 +1,3 @@
+// Using timestamps, find how many days have passed since the date of your birth. Feeling old, yet?
+
+// Write a function to find how many days have passed since any point in time (after 1970).
